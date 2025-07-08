21fc549befaa2effc8e13a7fba23bbe36437132a
Semwalakshit19 <akshitrs@redsecure.net>
2025-07-08 14:12:42 +0530

    Fix: some bugs

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let storedAccessToken = null;
 app.get("/linkedin/login", (req, res) => {
   const authUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${LINKEDIN_CLIENT_ID}&redirect_uri=${encodeURIComponent(
     REDIRECT_URI
-  )}&scope=w_member_social%20r_basicprofile%20w_organization_social`;
+  )}&scope=w_member_social`;
 
   res.redirect(authUrl);
 });
